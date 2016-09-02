f9220142da52acc41639106c119e5ed01ff7dd09
Mike Gibson <mike@mgdigital.co.uk>
2016-09-02 08:44:14 +0100

    Fixed namespace for emptying queue

--- lua/empty_queue.lua
+++ lua/empty_queue.lua
@@ -1,4 +1,4 @@
 local ns, queue = ARGV[1], ARGV[2]
 
-redis.call('DEL', ns..':'..queue..':queue', ':'..queue..':receiving', ':'..queue..':consuming', ':'..queue..':queued_ids')
+redis.call('DEL', ns..':'..queue..':queue', ns..':'..queue..':receiving', ns..':'..queue..':consuming', ns..':'..queue..':queued_ids')
 redis.call('SREM', ns..':queues', queue)
